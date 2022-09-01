f2301a91f0aac9d9cb21f25281de4587a48d571f
SwaggyFreddy <64239001+swaggyfreddy@users.noreply.github.com>
2022-09-01 15:31:10 +0800

    Update variables.tf
    Declaring all arguments as variables

--- variables.tf
+++ variables.tf
@@ -6,3 +6,33 @@ variable "secret_key" {
   type        = string
   description = "AWS Secret Key"
 }
+variable "ami" {
+  type = string
+  description = "AMI"
+}
+variable "instance_type" {
+  type = string
+  description = "Instance Type"
+}
+variable "subnet_id" {
+  type = string
+  description = "Instance Type"
+}
+variable "associate_public_ip_address" {
+  type = string
+  description = "Associated Public IP"
+}
+variable "associate_public_ip_address" {
+  type = string
+  default = "false"
+  description = "Associate Public IP Address?"
+}
+variable "vpc_security_group_ids" {
+  type = list(string)
+  description = "VPC Security Group IDs"
+}
+variable "tags" {
+  type        = list(map(string))
+  default     = []
+  description = "Tags"
+}
